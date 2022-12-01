09675c69c682ed4ffa73ce04761f96e561f2e3bd
dingfujing <dingfujing@zkbc.net>
2022-12-01 10:32:20 +0800

    增加常量类

---
+++ gen-admin/src/main/java/com/fj/gen/std/Constants.java
@@ -0,0 +1,15 @@
+package com.fj.gen.std;
+
+/**
+ * 常量类
+ *
+ * @author fjding
+ * @date 2022/12/1
+ */
+public final class Constants {
+
+    public static final String TRUE = "true";
+
+    public static final String FALSE = "false";
+}
+
